fbc538354d1387811dcb8a7cdf1062fd096228fe
Fabien Millerand <fab@millerf.com>
2018-03-10 13:20:04 +0100

    Design improvement

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,7 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     FormsModule,
-    VirtualListModule
+    VirtualListModule,
   ],
   providers: [
     DataManagerService,
